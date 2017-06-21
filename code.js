41c972691898816462e3caf87d0383ae8b16ed11
Cutwell <zacharysmith2000@googlemail.com>
2017-06-21 08:04:58 +0100

    added "did you mean" javascript

--- code.js
+++ code.js
@@ -1,3 +1,22 @@
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomInt(min, max) {return Math.floor(Math.random() * (max - min + 1)) + min;}
+
+function similarity(a,b){var c=a,d=b;a.length<b.length&&(c=b,d=a);var e=c.length;return 0==e?1:(e-editDistance(c,d))/parseFloat(e)}
+
+function editDistance(a,b){a=a.toLowerCase(),b=b.toLowerCase();for(var c=new Array,d=0;d<=a.length;d++){for(var e=d,f=0;f<=b.length;f++)if(0==d)c[f]=f;else if(f>0){var g=c[f-1];a.charAt(d-1)!=b.charAt(f-1)&&(g=Math.min(Math.min(g,e),c[f])+1),c[f-1]=e,e=g}d>0&&(c[b.length]=e)}return c[b.length]}
+
+function getClosestMatch(a){var b=window.location.href,c=0,d="";for(pageref in a){var e=similarity(b,pageref);top_match<e&&(c=e,d=pageref)}return d}
+
+function getClosestMatch(){
+  var current = window.location.href;
+  var page =""; //array loaded from pageref.txt
+  var top_match = "";
+  var top_match_score = 0;
+  for (pageref in page){
+    var score = similarity(current, pageref);
+    if (score > top_match_score){
+      top_match_score = score
+      top_match = pageref
+    }
+  }
+  return top_match
 }
